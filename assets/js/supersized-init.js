2651de2bc4ac83bfbbc792a8b41a120af7d84098
yangzai1 <minyangbai@sina.com>
2017-03-24 17:49:26 +0800

    denglutest

--- assets/js/supersized-init.js
+++ assets/js/supersized-init.js
@@ -19,11 +19,12 @@ jQuery(function($){
 
         // Components
         slide_links        : 'blank',    // Individual links for each slide (Options: false, 'num', 'name', 'blank')
-        slides             : [    // Slideshow Images
+        // Slideshow Images
+        /*slides             : [    
                                  {image : 'assets/img/backgrounds/1.jpg'},
                                  {image : 'assets/img/backgrounds/2.jpg'},
                                  {image : 'assets/img/backgrounds/3.jpg'}
-                       ]
+                       ]*/
 
     });
 
